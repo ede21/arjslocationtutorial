344a832c2c27a1d7a0ef9c40d0e801e019da10ec
Peter <Peter_Mainhardt@it-intelligent.com>
2020-06-11 13:46:52 +0200

    g

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
     button.innerText = '﹖';
 
+
+    //Button Click Event
+    document.querySelector('button[data-action="change"]').addEventListener('click', function () {
+        alert('test');
+    });
+
     //let places = staticLoadPlaces();
     //renderPlaces(places);
 };
